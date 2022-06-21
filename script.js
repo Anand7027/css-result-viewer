80e11f3d979310d55231d8c9de04cbaabf53f981
Anand7027 <81455577+Anand7027@users.noreply.github.com>
2022-06-21 10:25:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function color(){
+    var x = document.getElementById("result");
+    x.style.color = "red";
+}
+
+function font(){
+    var x = document.getElementById("result");
+    x . style.fontFamily = "calibri";
+    x. style. color = "blue";
+}
+ function magic(){
+    var x = document.getElementById("result");
+    x.style. display = "none";
+ }
+
+ function fontf(){
+    var x = document.getElementById("result");
+    x . style.fontFamily = "sans serif";
+    x. style. color = "purpal";
+}
